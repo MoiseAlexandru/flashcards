0589269b655771932e8a8173f5e8e6304f46abc9
Moise Alexandru <moise.alexandru.ichb@gmail.com>
2023-10-07 20:18:49 +0300

    finalizare proiect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import "./styles.css";
+import { questions } from "./data";
 
 export default function App() {
   return (
@@ -8,50 +10,24 @@ export default function App() {
   );
 }
 
-const questions = [
-  {
-    id: 3457,
-    question: "What language is React based on?",
-    answer: "JavaScript"
-  },
-  {
-    id: 7336,
-    question: "What are the building blocks of React apps?",
-    answer: "Components"
-  },
-  {
-    id: 8832,
-    question: "What's the name of the syntax we use to describe a UI in React?",
-    answer: "JSX"
-  },
-  {
-    id: 1297,
-    question: "How to pass data from parent to child components?",
-    answer: "Props"
-  },
-  {
-    id: 9103,
-    question: "How to give components memory?",
-    answer: "useState hook"
-  },
-  {
-    id: 2002,
-    question:
-      "What do we call an input element that is completely synchronised with state?",
-    answer: "Controlled element"
-  }
-];
+
 
 function FlashCards() {
 
     const [selectedId, setSelectedId] = useState(null);
     
+    function handleClick(id) {
+        if(selectedId === id)    // deselect
+            setSelectedId(null);
+        else
+            setSelectedId(id);   // select
+    }
 
     return (
         <div className = "flashcards">
             {questions.map((question) => (
-                <div key = {question.id}>
-                    <p>{question.question}</p>
+                <div key = {question.id} className = {question.id === selectedId ? "selected" : ""} onClick = {() => handleClick(question.id)}>
+                    <p>{question.id === selectedId ? question.answer : question.question}</p>
                 </div>
             ))}
         </div>
